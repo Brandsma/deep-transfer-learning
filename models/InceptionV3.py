0410568ac6ab53ea08e8e63c19da176082735220
Brandsma <a.brandsma@protonmail.com>
2021-02-17 15:22:19 +0100

    Forgot a bit of inception

--- models/InceptionV3.py
+++ models/InceptionV3.py
@@ -11,9 +11,7 @@ def build_inception(config):
     inputs = tf.keras.Input(shape=(299, 299, 3))
     x = preprocess_input(x)
     x = base_model(x, training=False)
-    x = global_average_layer(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+ #   x = global_average_layer(x)
+ #   x = tf.keras.layers.Dropout(0.2)(x)
     outputs = prediction_layer(x)
     model = tf.keras.Model(inputs, outputs)
-
-tf.keras.applications.inception_v3.preprocess_input
