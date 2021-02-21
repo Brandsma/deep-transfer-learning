ebddf1f9946d4ba16707e81e853a7ed9a0419a90
Brandsma <a.brandsma@protonmail.com>
2021-02-21 15:03:58 +0100

    Made an error, fixed it

--- models/util.py
+++ models/util.py
@@ -5,6 +5,6 @@ def get_optimizer(optimizer_type, learning_rate):
     optimizers = {
         "adam": tf.keras.optimizers.Adam,
         "adamax": tf.keras.optimizers.Adamax,
-        "RMS": tf.keras.optimizers.SGD,
+        "sgd": tf.keras.optimizers.SGD,
     }
     return optimizers[optimizer_type](learning_rate=learning_rate)
